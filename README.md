33533c4255dccd7472181daa11d8e4e787a9c158
sahyung <sahyung@sandbox.local>
2020-08-12 14:51:59 +0700

    minimum functionality

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # oorth-test-simple-chat
-simple chat application for backend developer position at Oorth Indonesia
+simple chat application for backend developer position at Oorth Indonesia using [express](https://expressjs.com/) and socket.io
 
 # Quickstart
 ```sh
 $ composer install
+$ npm install
+
+# run server
+$ npm run server
+
+# open another console to run frontend
 $ php artisan serve
 ```
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,130 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!--Socket.io scirpt-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
+    <title>{{ env('APP_NAME') }}</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+<body>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <!-- Modal content -->
+        <div class="modal-content">
+            <h1 class="modal-title">What's your nickname?</h1>
+            <input id="nickname-input" class="custom-input" type="text" />
+        </div>
+
+    </div>
+
+    <!--Big wrapper-->
+    <div class="big-wrapper">
+        <!-- Left Column-->
+        <div class="online-user-wrapper">
+            <div class="online-user-header-container">
+                <header>
+                    <h2>Online Users</h2>
+                </header>
+            </div>
+            <div>
+                <!--Online users goes here-->
+                <ul id="users-list">
+
+                </ul>
             </div>
         </div>
-    </body>
+        <!--Chat Wrapper -->
+        <div class="chat-wrapper">
+            <div class="simple-chat-title-container">
+                <header>
+                    <h1>{{ env('APP_NAME') }}</h1>
+                </header>
+            </div>
+
+            <!--Messages container-->
+            <div id="chatroom">
+                <div id="chatroom-history"></div>
+                <!--x is typing goes here-->
+                <div id="feedback"></div>
+            </div>
+
+            <!-- Input zone -->
+            <div id="input_zone">
+                <input id="message" class="vertical-align custom-input" type="text" />
+                <button id="send_message" class="vertical-align btn" type="button">Send</button>
+            </div>
+
+        </div>
+    </div>
+    <!--jQuery script-->
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <!--Scripts-->
+    <script src="./js/chat.js"></script>
+    <script src="./js/modalScript.js"></script>
+</body>
+
 </html>
